8c0506a5fc574b3590d329def8bc48d39a73b642
Rafael G Francisco <rafael.dev@hotmail.com>
2020-03-18 01:34:26 -0300

    add mock to AuthorizeApiRequest

--- spec/requests/clients_spec.rb
+++ spec/requests/clients_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Clients", type: :request do
   describe "GET /clients" do
+    before do
+      client = create(:client)
+
+      allow(AuthorizeApiRequest).to receive(:call).and_return(OpenStruct.new(result: client))
+    end
+
     it "works! (now write some real specs)" do
       get clients_path
+
       expect(response).to have_http_status(200)
     end
   end
