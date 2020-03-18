fada2f5b74028ecd5685cbba84c0468c06c778f2
Rafael G Francisco <rafael.dev@hotmail.com>
2020-03-18 01:34:11 -0300

    organize after generator

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,53 +1,53 @@
-class ClientsController < ApplicationController
-  before_action :set_client, only: [:show, :update, :destroy]
+# frozen_string_literal: true
 
-  # GET /clients
-  # GET /clients.json
+class ClientsController < ApplicationController
+  before_action :set_client, only: %i[show update destroy]
+
   def index
     @clients = Client.all
+
+    render json: @clients
   end
 
   # GET /clients/1
-  # GET /clients/1.json
   def show
+    render json: @client
   end
 
   # POST /clients
-  # POST /clients.json
   def create
     @client = Client.new(client_params)
 
     if @client.save
-      render :show, status: :created, location: @client
+      render json: @client, status: :created, location: @client
     else
       render json: @client.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /clients/1
-  # PATCH/PUT /clients/1.json
   def update
     if @client.update(client_params)
-      render :show, status: :ok, location: @client
+      render json: @client
     else
       render json: @client.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /clients/1
-  # DELETE /clients/1.json
   def destroy
     @client.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_client
-      @client = Client.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def client_params
-      params.fetch(:client, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_client
+    @client = Client.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def client_params
+    params.fetch(:client, {}).permit(:name, :email)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   post 'authenticate', to: 'authentication#authenticate'
+
+  resources :clients
 end
