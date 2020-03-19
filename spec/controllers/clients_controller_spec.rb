a270f7556daa0194bbeb11482ac785597b5e017f
Rafael G Francisco <rafael.dev@hotmail.com>
2020-03-19 00:36:39 -0300

    typo

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -29,11 +29,11 @@ RSpec.describe ClientsController, type: :controller do
   # Client. As you add validations to Client, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    skip('Add a hash of attributes valid for your model')
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    skip('Add a hash of attributes invalid for your model')
   }
 
   # This should return the minimal set of values that should be in the session
@@ -41,31 +41,31 @@ RSpec.describe ClientsController, type: :controller do
   # ClientsController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
-  describe "GET #index" do
-    it "returns a success response" do
+  describe 'GET #index' do
+    it 'returns a success response' do
       client = Client.create! valid_attributes
       get :index, params: {}, session: valid_session
       expect(response).to be_successful
     end
   end
 
-  describe "GET #show" do
-    it "returns a success response" do
+  describe 'GET #show' do
+    it 'returns a success response' do
       client = Client.create! valid_attributes
       get :show, params: {id: client.to_param}, session: valid_session
       expect(response).to be_successful
     end
   end
 
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Client" do
+  describe 'POST #create' do
+    context 'with valid params' do
+      it 'creates a new Client' do
         expect {
           post :create, params: {client: valid_attributes}, session: valid_session
         }.to change(Client, :count).by(1)
       end
 
-      it "renders a JSON response with the new client" do
+      it 'renders a JSON response with the new client' do
 
         post :create, params: {client: valid_attributes}, session: valid_session
         expect(response).to have_http_status(:created)
@@ -74,8 +74,8 @@ RSpec.describe ClientsController, type: :controller do
       end
     end
 
-    context "with invalid params" do
-      it "renders a JSON response with errors for the new client" do
+    context 'with invalid params' do
+      it 'renders a JSON response with errors for the new client' do
 
         post :create, params: {client: invalid_attributes}, session: valid_session
         expect(response).to have_http_status(:unprocessable_entity)
@@ -84,20 +84,20 @@ RSpec.describe ClientsController, type: :controller do
     end
   end
 
-  describe "PUT #update" do
-    context "with valid params" do
+  describe 'PUT #update' do
+    context 'with valid params' do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        skip('Add a hash of attributes valid for your model')
       }
 
-      it "updates the requested client" do
+      it 'updates the requested client' do
         client = Client.create! valid_attributes
         put :update, params: {id: client.to_param, client: new_attributes}, session: valid_session
         client.reload
-        skip("Add assertions for updated state")
+        skip('Add assertions for updated state')
       end
 
-      it "renders a JSON response with the client" do
+      it 'renders a JSON response with the client' do
         client = Client.create! valid_attributes
 
         put :update, params: {id: client.to_param, client: valid_attributes}, session: valid_session
@@ -106,8 +106,8 @@ RSpec.describe ClientsController, type: :controller do
       end
     end
 
-    context "with invalid params" do
-      it "renders a JSON response with errors for the client" do
+    context 'with invalid params' do
+      it 'renders a JSON response with errors for the client' do
         client = Client.create! valid_attributes
 
         put :update, params: {id: client.to_param, client: invalid_attributes}, session: valid_session
@@ -117,8 +117,8 @@ RSpec.describe ClientsController, type: :controller do
     end
   end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested client" do
+  describe 'DELETE #destroy' do
+    it 'destroys the requested client' do
       client = Client.create! valid_attributes
       expect {
         delete :destroy, params: {id: client.to_param}, session: valid_session
